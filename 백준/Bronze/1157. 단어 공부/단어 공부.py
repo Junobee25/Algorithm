ee918da1eff1654235e628c1672f07309056b149
Junobee25 <109403631+Junobee25@users.noreply.github.com>
2022-09-27 21:49:09 +0900

    [Bronze I] Title: 단어 공부, Time: 108 ms, Memory: 32796 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -0,0 +1,14 @@
+word = input().upper()
+word_list = list(set(word))
+word_list
+count_list = []
+
+for i in word_list:
+    cnt = word.count(i)
+    count_list.append(cnt)
+    
+if count_list.count(max(count_list)) > 1:
+    print('?')
+else:
+    max_index = count_list.index(max(count_list))
+    print(word_list[max_index])
