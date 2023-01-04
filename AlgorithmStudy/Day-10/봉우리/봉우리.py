566c9c4fa5d46e4d036c73401d510c3188ca897e
Junobee25 <wnsdhqo@naver.com>
2023-01-04 16:49:02 +0900

    SOL - 상하좌우 탐색

---
+++ AlgorithmStudy/Day-10/봉우리/봉우리.py
@@ -0,0 +1,16 @@
+T = int(input())  # 격자판 크기
+arr = [list(map(int,input().split())) for _ in range(T)]  # 이차원 배열의 격자판 배열 생성
+
+arr.insert(0,[0]*(T+2))  # 0으로 둘러싸기 
+arr.append([0]*(T+2))
+
+for i in range(T):
+    arr[i+1].insert(0,0)
+    arr[i+1].append(0)
+cnt = 0
+for i in range(1,T+1):
+    for j in range(1,T+1):  ## 현재위치,상,하,좌,우중 최대값이 현재위치값과 같다면 봉우리 count
+        if arr[i][j] > max(arr[i-1][j],arr[i+1][j],arr[i][j-1],arr[i][j+1]) :
+            cnt += 1
+
+print(cnt)
