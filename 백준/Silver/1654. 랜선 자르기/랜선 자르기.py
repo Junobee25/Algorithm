f1b32a29521cbfa9428da046bfd7ca2b6243d177
배준오 <109403631+Junobee25@users.noreply.github.com>
2023-01-08 23:58:02 +0900

    [Silver II] Title: 랜선 자르기, Time: 124 ms, Memory: 114328 KB -BaekjoonHub

---
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.py
@@ -0,0 +1,31 @@
+import sys
+input = sys.stdin.readline
+def Count(len):
+    cnt = 0
+    for x in arr:
+        cnt += (x//len)
+    return cnt
+
+N,M = map(int,input().split())
+arr = []
+res = 0
+largest = 0
+for i in range(N):
+    tmp = int(input())
+    arr.append(tmp)
+    largest = max(largest, tmp)
+
+
+lt = 1
+rt = largest
+
+solution = []
+while lt<=rt:
+    mid = (lt+rt)//2
+    if Count(mid)>=M:
+        res = mid
+        lt = mid + 1
+    else:
+        rt = mid - 1
+
+print(res)            
