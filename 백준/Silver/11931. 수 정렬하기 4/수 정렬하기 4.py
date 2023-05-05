a2bb55f09b046421896e6d3f553edf2c01a1fd4b
배준오 <109403631+Junobee25@users.noreply.github.com>
2023-05-05 22:18:27 +0900

    [Silver V] Title: 수 정렬하기 4, Time: 1528 ms, Memory: 77076 KB -BaekjoonHub

--- 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
+++ 백준/Silver/11931. 수 정렬하기 4/수 정렬하기 4.py
@@ -1,7 +1,9 @@
 n = int(input())
+import sys
+input = sys.stdin.readline
 arr = []
 for i in range(n):
-    a = int(input())
+    a = int(input().rstrip())
     arr.append(a)
 
 arr.sort(reverse=True)
