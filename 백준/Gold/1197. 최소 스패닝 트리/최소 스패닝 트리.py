c8c0f896ecb17a3255b3aeec13a631f0744a7c8c
배준오 <109403631+Junobee25@users.noreply.github.com>
2023-05-07 01:15:53 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 552 ms, Memory: 132008 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,37 @@
+n,m = map(int,input().split())
+
+
+def find_parent(parent,x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent,parent[x])
+    return parent[x]
+
+def union_find(parent,a,b):
+    a = find_parent(parent,a)
+    b = find_parent(parent,b)
+    if a < b :
+        parent[b] = a
+    else:
+        parent[a] = b
+
+edges = []
+parent = [0]*(n+1)
+
+# 부모노드 자기자신
+for i in range(1,n+1):
+    parent[i] = i
+
+# 노드,간선 정보와 비용
+for _ in range(m):
+    a,b,cost = map(int,input().split())
+    edges.append((cost,a,b))
+
+edges.sort()
+result = 0
+for edge in edges:
+    cost,a,b = edge
+    if find_parent(parent,a) != find_parent(parent,b):
+        union_find(parent,a,b)
+        result += cost
+
+print(result)
