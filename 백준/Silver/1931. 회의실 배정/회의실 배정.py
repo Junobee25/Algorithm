a9d110d0f32a912d3c7bcff4cb5db850d3765eef
배준오 <109403631+Junobee25@users.noreply.github.com>
2023-08-17 00:15:00 +0900

    [Silver I] Title: 회의실 배정, Time: 6752 ms, Memory: 58420 KB -BaekjoonHub

--- 백준/Silver/1931. 회의실 배정/회의실 배정.py
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -1,13 +1,23 @@
-N = int(input())
-meeting = []
-for i in range(N):
-    start, end = map(int, input().split())
-    meeting.append((start, end))
-meeting.sort(key=lambda x: (x[1],x[0]))
-end_time = 0
-cnt = 0
-for start,end in meeting:
-    if start>=end_time:
-        end_time = end
+n = int(input())
+
+arr = []
+
+cnt = 1
+
+
+
+for i in range(n):
+    arr.append(list(map(int,input().split())))
+
+arr.sort(key = lambda x : (x[1],x[0]))
+
+end_time = arr[0][1]
+
+
+for i in range(1,n):
+    if end_time <= arr[i][0]:
         cnt += 1
+        end_time = arr[i][1]
+        
 print(cnt)
+
