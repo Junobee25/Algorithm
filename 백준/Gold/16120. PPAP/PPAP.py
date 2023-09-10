73dc179b3c62ba1d1a92b042b5701fef04a21ca6
배준오 <109403631+Junobee25@users.noreply.github.com>
2023-09-11 01:48:31 +0900

    [Gold IV] Title: PPAP, Time: 504 ms, Memory: 38308 KB -BaekjoonHub

---
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -0,0 +1,14 @@
+string = input()
+
+stack = []
+for i in range(len(string)):
+    stack.append(string[i])
+    if stack[-4:] == ["P","P","A","P"]:
+        for j in range(4):
+            stack.pop()
+        stack.append("P")
+
+if stack == ["P","P","A","P"] or stack == ["P"]:
+    print('PPAP')
+else:
+    print('NP')
