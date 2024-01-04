04ff7272d01bdb54e3cdfc540a6df2d7ec535450
Juno Bae <109403631+Junobee25@users.noreply.github.com>
2024-01-04 12:54:25 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,18 +1,29 @@
-T = int(input())
+import sys
 
-for i in range(T):
-    arr = list(input())
-    cnt = 0
-    for j in arr:
-        if j == '(':
-            cnt +=1
-        elif j == ')':
-            cnt -=1
-        if cnt < 0:
-            print('NO')
-            break
-    if cnt >0 :
-        print('NO')
-    elif cnt == 0:
-        print("YES")
+T = int(sys.stdin.readline())
+OPEN_BRACKET = "("
+CLOSE_BRACKET = ")"
+NO = "NO"
+YES = "YES"
 
+for _ in range(T):
+    bracket_list = list(sys.stdin.readline().rstrip())
+    stack = []
+
+    if bracket_list[0] == CLOSE_BRACKET:
+        print(NO)
+        continue
+
+    for idx in range(len(bracket_list)):
+        if bracket_list[idx] == OPEN_BRACKET:
+            stack.append(OPEN_BRACKET)
+        else:
+            if stack and stack[-1] == OPEN_BRACKET:
+                stack.pop()
+            else:
+                stack.append(CLOSE_BRACKET)
+
+    if not stack:
+        print(YES)
+    else:
+        print(NO)
