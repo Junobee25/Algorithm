797f56df3555551893552b74b606b62397454ce2
Juno Bae <109403631+Junobee25@users.noreply.github.com>
2024-06-03 08:54:57 +0900

    [Gold IV] Title: 로또, Time: 412 ms, Memory: 72716 KB -BaekjoonHub

---
+++ 백준/Gold/2758. 로또/로또.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+    public static long[][] mem;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        for (int t = 0; t < tc; t++) {
+            int n = sc.nextInt(), m = sc.nextInt();
+            mem = new long[n + 1][m + 1];
+
+            for (int i = 0; i <= n; i++) {
+                for (int j = 0; j <= m; j++) {
+                    mem[i][j] = -1;
+                }
+            }
+            System.out.println(solve(n,m));
+        }
+    }
+    public static long solve(int i, int last) {
+        // base case
+        if(last <= 0) return 0;
+        if(i == 1) return last;
+
+        // recursive case
+        if(mem[i][last] == -1) {
+            mem[i][last] = solve(i-1, last / 2) + solve(i, last - 1);
+        }
+        return mem[i][last];
+    }
+}
