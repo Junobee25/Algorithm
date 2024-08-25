ca543622f2360d3e027040e3edb5d14e5e3336af
Juno Bae <109403631+Junobee25@users.noreply.github.com>
2024-08-25 17:55:24 +0900

    [Silver I] Title: 카잉 달력, Time: 3604 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/6064. 카잉 달력/카잉 달력.py
@@ -0,0 +1,14 @@
+t = int(input())
+
+for _ in range(t):
+    m, n, x, y = map(int,input().split())
+    
+    k = x
+    
+    while k <= m * n:
+        if (k-x) % m == 0 and (k-y) % n == 0:
+            print(k)
+            break
+        k += m
+    else:
+        print(-1)
