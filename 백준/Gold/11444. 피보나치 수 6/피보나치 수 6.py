dfcc160464bdcffc7525f972ee8ab111252ee2aa
Juno Bae <109403631+Junobee25@users.noreply.github.com>
2025-01-28 11:03:55 +0900

    [Gold II] Title: 피보나치 수 6, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,25 @@
+n = int(input())
+
+matrix = [[1,1],[1,0]]
+
+def mult_matrix(a,b):
+    arr = [[0,0],[0,0]]
+    for k in range(2):
+        for i in range(2):
+            for j in range(2):
+                arr[i][j] += a[i][k]*b[k][j]
+                arr[i][j] %= 1000000007
+    return arr
+
+
+def mult(n,matrix):
+    if n == 1:
+        return matrix
+    temp = mult(n//2, matrix)
+    if n % 2:
+        return mult_matrix(matrix, mult_matrix(temp, temp))
+    else:
+        return mult_matrix(temp, temp)    
+    
+
+print(mult(n, matrix)[0][1])
