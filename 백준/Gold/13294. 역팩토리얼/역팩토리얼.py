786f41b852b3a1f05351bd5fe37603b1f11355e8
Juno Bae <109403631+Junobee25@users.noreply.github.com>
2025-02-17 15:34:16 +0900

    [Gold III] Title: 역팩토리얼, Time: 360 ms, Memory: 130844 KB -BaekjoonHub

--- 백준/Gold/13294. 역팩토리얼/역팩토리얼.py
+++ 백준/Gold/13294. 역팩토리얼/역팩토리얼.py
@@ -1,14 +1,23 @@
 from math import log, pi
+
 def logfact(x):
-    return x*(log(x)-1) + log(2*pi*x)/2 + 1/(12*x)
-nfact = int(input())
+    return x*(log(x) - 1) + log(2*pi*x)/2 + 1/(12*x)
+
+nf = int(input())
+
 data = {1:1, 2:2, 6:3, 24:4, 120:5, 720:6, 5040:7, 40320:8, 362880:9, 3628800:10, 39916800:11, 479001600:12, 6227020800:13, 87178291200:14, 1307674368000:15, 20922789888000:16, 355687428096000:17, 6402373705728000:18, 121645100408832000:19}
-if nfact in data.keys(): print(data[nfact]); quit()
-start,end,mid = 20,10**6,500000
+if nf in data.keys(): print(data[nf]); quit()
+
+start, end, mid = 20, 10**6, 500000
 error = float(1e-3)
-x = log(nfact)
+
+x = log(nf)
+
 while True:
-    mid = (start+end) // 2
-    if logfact(mid) > x + error: end = mid
-    elif logfact(mid) < x - error: start = mid
-    else: print(mid); quit()
+    mid  = (start + end) // 2
+    if logfact(mid) > x + error:
+        end = mid
+    elif logfact(mid) < x - error:
+        start = mid
+    else:
+        print(mid); quit()
